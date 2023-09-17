37baac58b8ddae351a39b707e54ccf2289067795
AltESV <ishigami.daniel@gmail.com>
2023-09-17 11:39:38 +0100

    updated error handling

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');    
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
 const app = express();
-const tourRouter = require('./routes/tourRoutes')
-const userRouter = require('./routes/userRoutes')
 
 //MIDDLEWARE
 if(process.env.NODE_ENV === 'development') {
@@ -12,10 +16,6 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`))
 
-app.use((req, res, next) => {
-    console.log('hello from middleware👋');
-    next();
-})
 
 app.use((req, res, next) =>{
     req.requestTime =  new Date().toISOString();
@@ -25,6 +25,10 @@ app.use((req, res, next) =>{
 //ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
-
+app.all('*', (req, res, next) => {
+    next(new AppError(`route ${req.originalUrl} does not exist on this server`, 404));
+})
+//ERROR HANDLING 
+app.use(globalErrorHandler);
 
 module.exports = app;
